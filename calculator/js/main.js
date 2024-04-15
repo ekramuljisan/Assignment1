062c2490934c7323caa2d5f19abee77b3c48de0d
MD Ekramul Islam <153128271+ekramuljisan@users.noreply.github.com>
2024-04-15 19:06:05 +0600

    Update main.js

--- calculator/js/main.js
+++ calculator/js/main.js
@@ -7,26 +7,18 @@ let calculator=()=>{
 
     if(Number.parseFloat(num1) && Number.parseFloat(num2)){
         if(calculate==="+"){
-            let num1=parseFloat(document.getElementById("number1").value);
-            let num2=parseFloat(document.getElementById("number2").value);
             document.getElementById("result").innerHTML="Result: "+(num1+num2);
             document.getElementById("result").style.backgroundColor="#D1E7DD";
         }
         else if(calculate==="-"){
-        let num1=parseFloat(document.getElementById("number1").value);
-        let num2=parseFloat(document.getElementById("number2").value);
         document.getElementById("result").innerHTML="Result: "+(num1-num2);
         document.getElementById("result").style.backgroundColor="#D1E7DD"; 
         }
         else if(calculate==="*"){
-            let num1=parseFloat(document.getElementById("number1").value);
-            let num2=parseFloat(document.getElementById("number2").value);
             document.getElementById("result").innerHTML="Result: "+(num1*num2);
             document.getElementById("result").style.backgroundColor="#D1E7DD";
         }   
         else{
-            let num1=parseFloat(document.getElementById("number1").value);
-            let num2=parseFloat(document.getElementById("number2").value);
             document.getElementById("result").innerHTML="Result: "+(num1/num2);
             document.getElementById("result").style.backgroundColor="#D1E7DD"; 
         }
